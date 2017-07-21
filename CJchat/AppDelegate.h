07831b10b73643bc7e7df743498cb7a1fc2cea10
ChenJun <chen_jun@honestsoft.cn>
2017-07-21 15:47:08 +0800

    Initial Commit

---
+++ CJchat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJchat
+//
+//  Created by Chen_Jun on 2017/7/21.
+//  Copyright © 2017年 zwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJchat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJchat
+//
+//  Created by Chen_Jun on 2017/7/21.
+//  Copyright © 2017年 zwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
